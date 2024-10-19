c2587d7a44e1c8d32afa6aa28a801485552802aa
Orion <orioncheetah@gmail.com>
2024-10-19 18:18:26 -0400

    header wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
-
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-    const [count, setCount] = useState("$");
+    const [count, setCount] = useState(0);
 
     return (
-        <>
+        <div className="w-screen justify-center items-center">
+            <Header />
             <div className=""></div>
+
             <h1>Lemon Window</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     GET A QUOTE {count}
                 </button>
             </div>
-        </>
+        </div>
     );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <div className="mx-auto bg-[#FFF9EC]/80 w-screen h-[380px] bg-noise">
+            Lemon Window
+        </div>
+    );
+}
+export default Header;
