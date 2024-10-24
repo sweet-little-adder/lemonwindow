07ad742935a4e1dbd4437fd9f631ba6a6f5f29ee
Orion <orioncheetah@gmail.com>
2024-10-24 18:52:09 -0400

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import "../App.css";
 function Header() {
     return (
-        <div className="mx-auto bg-[#FFF9EC]/80 w-screen h-[380px] bg-noise">
-            <div className="pt-7 font-streetwear text-5xl text-[#FFE600] drop-shadow!">
-                Lemon
-            </div>{" "}
-            Window Cleaning
+        <div className="mx-auto bg-[#FFF9EC]/80 w-screen h-[380px] bg-noise text-4xl">
+            <div>
+                <div className="mb-3 pt-7 font-streetwear text-[128px] text-[#FFE600] drop-shadow!">
+                    Lemon
+                </div>
+                Window Cleaning
+            </div>
+            <div className="flex justify-around items-center w-screen">
+                <div>Service</div>
+                <div>About</div>
+                <div>Locations</div>
+            </div>
         </div>
     );
 }
