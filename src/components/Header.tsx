431a1308b1db03469a89580b871ca6caa2e0ba9f
Orion <orioncheetah@gmail.com>
2024-10-24 23:06:15 -0400

    change color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,19 +16,19 @@ const Header: React.FC<Props> = ({ quote, setQuote }) => {
                 Window Cleaning
             </div>
             <div className="flex justify-around items-center w-screen text-[36px] mt-12">
-                <div className="cursor-pointer transition-all hover:drop-shadow-[0_6px_10px_rgba(0,0,0,0.25)]">
+                <div className="cursor-pointer transition-all hover:underline">
                     Service
                 </div>
-                <div className="cursor-pointer transition-all hover:drop-shadow-[0_6px_10px_rgba(0,0,0,0.25)]">
+                <div className="cursor-pointer transition-all hover:underline">
                     About
                 </div>
-                <div className="cursor-pointer transition-all hover:drop-shadow-[0_6px_10px_rgba(0,0,0,0.25)]">
+                <div className="cursor-pointer transition-all hover:underline">
                     Locations
                 </div>
             </div>
             <button
                 onClick={() => setQuote(!quote)}
-                className=" md:text-[48px] mt-8 transition-all hover:drop-shadow-[0_6px_10px_rgba(255,0,170,0.25)]"
+                className=" md:text-[48px] mt-8 transition-all hover:drop-shadow-[0_0px_10px_rgba(255,156,156,0.5)]"
             >
                 GET A QUOTE
             </button>
