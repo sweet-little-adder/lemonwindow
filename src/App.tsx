504ffc020372c62c33ab8f140e768daa421a4818
Orion <orioncheetah@gmail.com>
2024-11-06 22:28:18 -0500

    residential/commercial

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,27 @@ function App() {
             {quote && <Quote setQuote={setQuote} />}
             <div className="z-10 mt-24 mx-auto  w-full h-fit flex justify-center text-4xl">
                 {page === "services" && (
-                    <div className="flex flex-wrap sm:gap-9 mx-auto/ justify-center sm:space-y-0 space-y-20 h-fit">
-                        <div className="space-y-6">
-                            <u>Exterior</u>
-                            <br />
-                            <p>window cleaning</p>
-                            <p>house wash</p>
-                            <p>roof wash</p>
-                            <p>guttercleaning</p>
-                        </div>
-                        <div className="space-y-6">
-                            <u>Interior</u>
-                            <br />
-                            <p>carpet cleaning</p>
-                            <p>tile and grout</p>
+                    <div className="flex-col items-center text-[#555555]">
+                        <b className="bg-[#555] text-[#fff9ec]  p-2 pt-4 rounded-lg/">
+                            Residential / Commercial
+                        </b>
+                        <br />
+                        <br />
+                        <div className="flex flex-wrap sm:gap-9 mx-auto/ justify-center sm:space-y-0 space-y-20 h-fit">
+                            <div className="space-y-6">
+                                <u>Exterior</u>
+                                <br />
+                                <p>window cleaning</p>
+                                <p>house wash</p>
+                                <p>roof wash</p>
+                                <p>guttercleaning</p>
+                            </div>
+                            <div className="space-y-6">
+                                <u>Interior</u>
+                                <br />
+                                <p>carpet cleaning</p>
+                                <p>tile and grout</p>
+                            </div>
                         </div>
                     </div>
                 )}
